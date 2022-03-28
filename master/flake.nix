1af28e31d521358617a89fcb645ddfa3a6f12df5
hugosenari <hugosenari@gmail.com>
2022-03-27 21:57:26 -0300

    chore: re index treesitter_rust

--- master/flake.nix
+++ master/flake.nix
@@ -11,8 +11,8 @@
   inputs.src-treesitter_rust-master.ref   = "refs/heads/master";
   inputs.src-treesitter_rust-master.owner = "genotrance";
   inputs.src-treesitter_rust-master.repo  = "nimtreesitter";
-  inputs.src-treesitter_rust-master.dir   = "treesitter_rust";
-  inputs.src-treesitter_rust-master.type  = "github";
+    inputs.src-treesitter_rust-master.dir   = "treesitter_rust";
+inputs.src-treesitter_rust-master.type  = "github";
   
   inputs."nimgen".owner = "nim-nix-pkgs";
   inputs."nimgen".ref   = "master";
